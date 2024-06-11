6de98f5f8cfb0103621c7745b10e87aa77f68126
tkarnani <tkarnani@ucsd.edu>
2024-06-11 15:44:57 -0700

    util

--- motifseeker/motifseekv0.py
+++ motifseeker/motifseekv0.py
@@ -523,7 +523,7 @@ if ((args.inputfile is not None) and (args.genome is not None)):
 
 
         # Store HOMER motifs in var motifs
-        motifs = ParseMotifsFile("../motifs/custom.motifs")
+        motifs = ParseMotifsFile("motifs/custom.motifs")
         null_dist = null_dist = [
         0.1, 0.2, 0.15, 0.3, 0.25, 0.35, 0.4, 0.45, 0.5, 0.55,
         0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05,
